ad1ea9839339ce8ff9e8d1484b1c5bb240f98385
Maria-Bayskikh <masya_30_06@mail.ru>
2023-11-19 20:13:10 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] fibi = new int[15];
+        fibi[0]=0;
+        fibi[1]=1;
+        fibi[2]=1;
+        fibi[3]=2;
+        fibi[4]=3;
+        fibi[5]=5;
+        fibi[6]=8;
+        fibi[7]=13;
+        fibi[8]=21;
+        fibi[9]=34;
+        fibi[10]=55;
+        fibi[11]=89;
+        fibi[12]=144;
+        fibi[13]=233;
+        fibi[14]=377;
+        System.out.println(fibi[0]);
+        System.out.println(fibi[2]);
+        System.out.println(fibi[4]);
+        System.out.println(fibi[6]);
+        System.out.println(fibi[8]);
+        System.out.println(fibi[12]);
+        System.out.println(fibi[14]);
+        }
+    }
